95d06c5ca9426dd2d07ebcc6a79b15fc2c7f6e4f
Sergio <sergiovmml@gmail.com>
2021-09-19 10:59:19 -0300

    1

---
+++ src/pages/Home/index.js
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput
+} from 'react-native';
+import { ButtonComp } from '../../components/ButtonComp';
+import { SkillCard } from '../../components/SkillCard';
+
+
+
+export function Home(){
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState([]);
+
+  function handleAddNewSkill(){
+    setMySkills(oldState => [...oldState, newSkill]);
+  }
+
+
+  return (
+      <View style={styles.container}>
+        <TextInput style={styles.title}
+            style={styles.input}
+            placeholder="New skill"
+            placeholderTextColor="#555"
+            onChangeText={setNewSkill}
+        />
+        
+        <ButtonComp />
+      
+
+        <Text style={[styles.title, { marginVertical: 50}]}>
+            My Skills    
+        </Text>
+
+        {
+            mySkills.map(skill => (
+                <SkillCard />
+            ))
+        }
+
+
+      </View>
+  )
+      }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#121015',
+    paddingHorizontal: 20,
+    paddingVertical: 70,
+    paddingHorizontal: 30
+},
+title: {
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold'
+},
+input: {
+    backgroundColor: '#1F1E25',
+    color: '#FFF',
+    fontSize: 18,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+    marginTop: 30,
+    borderRadius: 7
+},
+button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20
+},
+buttonText: {
+    color: '#FFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+},
+buttonSkill: {
+    backgroundColor: '#1F1E25',
+    padding: 15,
+    borderRadius: 50,
+    alignItems: 'center',
+    marginVertical: 10
+},
+textSkill: {
+    color: '#FFF',
+    fontSize: 22,
+    fontWeight: 'bold',
+}
+}) 
